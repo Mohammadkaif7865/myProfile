ee75b1cd3aff9393e69c788e00e21d0d2ffb648b
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-21 22:17:39 +0530

    yes me

--- src/Component/Message.js
+++ src/Component/Message.js
@@ -5,10 +5,11 @@ function Message() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [messageSent, setMessageSent] = useState("");
     const sendMessage = (e) => {
         e.preventDefault();
-        console.log("called");
         const data = {
+            website: "profilewebsite",
             name: name,
             email: email,
             message: message
@@ -20,24 +21,27 @@ function Message() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
-        }).then((res) => res.json()).then((data) => console.log(data.acknowledged))
+        }).then((res) => res.json()).then((data) => setMessageSent(data.acknowledged))
     }
     return (
         <>
             <div className="forsep"></div>
             <div className="container">
                 <form onSubmit={sendMessage}>
+                    {
+                        messageSent ? <span style={{color:"green"}} >message recieved </span> : <span style={{color:"red"}} >message not recieved</span>
+                    }
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={name} onChange={(e) => setName(e.target.value)} />
                         <div id="emailHelp" className="form-text">I'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputName1" className="form-label">Name</label>
+                        <label htmlFor="exampleInputName1" className="form-label">Name</label>
                         <input type="text" className="form-control" id="exampleInputName1" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputMessage" className="form-label">Message</label>
+                        <label htmlFor="exampleInputMessage" className="form-label">Message</label>
                         <input type="textarea" className="form-control" id="exampleInputMessage" value={message} onChange={(e) => setMessage(e.target.value)} />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
--- src/Footer.js
+++ src/Footer.js
@@ -5,25 +5,25 @@ function Footer() {
         <>
             <footer>
 
-                <div class="foot-ele" id="contacts">
-                    <a href="https://github.com/Mohammadkaif7865" target="_blank">
-                    <i class="bi bi-github"></i>
+                <div className="foot-ele" id="contacts">
+                    <a href="https://github.com/Mohammadkaif7865" rel='noopener'>
+                    <i className="bi bi-github"></i>
                     </a>
-                    <a href="https://twitter.com/Mdkaif78605" target="_blank">
-                    <i class="bi bi-twitter"></i>
+                    <a href="https://twitter.com/Mdkaif78605" rel='noopener'>
+                    <i className="bi bi-twitter"></i>
                     </a>
-                    <a href="https://www.mohammadkaif051197@gmail.com" target="_blank"><i class="bi bi-gmail"></i></a>
-                    <a href="https://www.facebook.com/profile.php?id=100047805708813" target="_blank">
-                    <i class="bi bi-meta"></i>
+                    <a href="https://www.mohammadkaif051197@gmail.com" rel='noopener'><i className="bi bi-gmail"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100047805708813" rel='noopener'>
+                    <i className="bi bi-meta"></i>
                     </a>
-                    <a href="https://www.instagram.com/mohammadkaif403/" target="_blank">
-                    <i class="bi bi-instagram"></i>
+                    <a href="https://www.instagram.com/mohammadkaif403/" rel='noopener'>
+                    <i className="bi bi-instagram"></i>
                     </a>
-                    <a href="https://www.linkedin.com/in/mohammad-kaif-99aaba1b9/" target="_blank">
-                    <i class="bi bi-linkedin"></i>
+                    <a href="https://www.linkedin.com/in/mohammad-kaif-99aaba1b9/" rel='noopener'>
+                    <i className="bi bi-linkedin"></i>
                     </a>
                 </div>
-                <div class="foot-ele">
+                <div className="foot-ele">
                     <p>
                         <sup>&copy;</sup>Copyright 2022, <sup>&#xae;</sup>All Rights Reserved. @ Mohammad kaif 0.0.1
                     </p>

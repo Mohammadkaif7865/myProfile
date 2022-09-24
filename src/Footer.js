ac31ec505209eca9b12d8a1c5a32e89a4b705478
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-24 19:58:50 +0530

    yes me

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Footer.css";
 function Footer() {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true);
+        } else if (scrolled <= 300) {
+            setVisible(false);
+        }
+    };
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+            /* you can also use 'auto' behaviour
+                     in place of 'smooth' */
+        });
+    };
+    window.addEventListener("scroll", toggleVisible);
     return (
         <>
             <footer>
-
+                <div
+                    className="float-b-t-t"
+                    style={{ display: visible ? "inline" : "none" }}
+                >
+                    <h3>
+                        <i className="bi bi-chevron-up" onClick={scrollToTop} style={{ position: "relative", top: "-13px", left: "-7px" }}></i>
+                    </h3>
+                </div>
                 <div className="foot-ele" id="contacts">
                     <a href="https://github.com/Mohammadkaif7865" rel='noopener'>
-                    <i className="bi bi-github"></i>
+                        <i className="bi bi-github"></i>
                     </a>
                     <a href="https://twitter.com/Mdkaif78605" rel='noopener'>
-                    <i className="bi bi-twitter"></i>
+                        <i className="bi bi-twitter"></i>
                     </a>
                     <a href="https://www.mohammadkaif051197@gmail.com" rel='noopener'><i className="bi bi-gmail"></i></a>
                     <a href="https://www.facebook.com/profile.php?id=100047805708813" rel='noopener'>
-                    <i className="bi bi-meta"></i>
+                        <i className="bi bi-meta"></i>
                     </a>
                     <a href="https://www.instagram.com/mohammadkaif403/" rel='noopener'>
-                    <i className="bi bi-instagram"></i>
+                        <i className="bi bi-instagram"></i>
                     </a>
                     <a href="https://www.linkedin.com/in/mohammad-kaif-99aaba1b9/" rel='noopener'>
-                    <i className="bi bi-linkedin"></i>
+                        <i className="bi bi-linkedin"></i>
                     </a>
                 </div>
                 <div className="foot-ele">

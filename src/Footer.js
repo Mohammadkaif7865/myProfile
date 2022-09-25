221077d0ac5ca7cd8b1b082d897e2ad7661720f5
mohammad kaif <mohammadkaif051197@gmail.com>
2022-09-25 19:12:44 +0530

    uyes me

--- src/Footer.js
+++ src/Footer.js
@@ -32,20 +32,20 @@ function Footer() {
                     </h3>
                 </div>
                 <div className="foot-ele" id="contacts">
-                    <a href="https://github.com/Mohammadkaif7865" rel='noopener'>
+                    <a href="https://github.com/Mohammadkaif7865" target="_blank">
                         <i className="bi bi-github"></i>
                     </a>
-                    <a href="https://twitter.com/Mdkaif78605" rel='noopener'>
+                    <a href="https://twitter.com/Mdkaif78605" target="_blank">
                         <i className="bi bi-twitter"></i>
                     </a>
-                    <a href="https://www.mohammadkaif051197@gmail.com" rel='noopener'><i className="bi bi-gmail"></i></a>
-                    <a href="https://www.facebook.com/profile.php?id=100047805708813" rel='noopener'>
+                    <a href="https://www.mohammadkaif051197@gmail.com" target="_blank"><i className="bi bi-gmail"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100047805708813" target="_blank">
                         <i className="bi bi-meta"></i>
                     </a>
-                    <a href="https://www.instagram.com/mohammadkaif403/" rel='noopener'>
+                    <a href="https://www.instagram.com/mohammadkaif403/" target="_blank">
                         <i className="bi bi-instagram"></i>
                     </a>
-                    <a href="https://www.linkedin.com/in/mohammad-kaif-99aaba1b9/" rel='noopener'>
+                    <a href="https://www.linkedin.com/in/mohammad-kaif-99aaba1b9/" target="_blank">
                         <i className="bi bi-linkedin"></i>
                     </a>
                 </div>
